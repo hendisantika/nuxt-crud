8a79bfabd6f2d3e1521039c35a037acc1207fae0
hendisantika <hendisantika@yahoo.co.id>
2024-02-03 06:40:32 +0700

    Add index.ts types ✏️📗 :octocat:⬆

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export interface IAuthor {
+    name: string;
+    _id?: string;
+}
+
+export interface IBook {
+    _id?: string;
+    title: string;
+    isbn: string;
+    pageCount: number;
+    published: string;
+    author: IAuthor;
+}
