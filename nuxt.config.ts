d93b75ec851cb0d1c40437e77ae2919b64a7fedc
hendisantika <hendisantika@yahoo.co.id>
2024-02-03 06:49:03 +0700

    Modify nuxt.config.ts ✏️📗 :octocat:⬆

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true}
-})
+    devtools: {enabled: true},
+    runtimeConfig: {
+        MONGO_URI: process.env.MONGO_URI
+    },
+    nitro: {
+        plugins: ['@/server/db/index.ts']
+    },
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
+
+    build: {
+        transpile: ['@headlessui/vue', 'vue-toastification', '@headlessui/tailwindcss']
+    },
+
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    }
+});
